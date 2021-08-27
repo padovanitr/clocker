f1aa0c5ecaa4deecbb3cdcb5007fe208513347ab
Thiago Padovani <padovanitr@gmail.com>
2021-08-26 23:12:57 -0300

    Agenda problem

--- components/Agenda/index.js
+++ components/Agenda/index.js
@@ -1,7 +1,7 @@
 import { Button } from '@chakra-ui/react';
 import firebase from '../../config/firebase';
 
-export default function Agenda() {
+export const Agenda = () => {
     const logout = () => firebase.auth().signOut()
     return (
       <div>
